47741acf3dfdd9a88bff90917c11eeb9aedd5647
Matias Piipari <matias.piipari@gmail.com>
2016-05-13 01:54:44 +0100

    Initial commit: Adds GRT library build + headers.

---
+++ BodyLanguage/BodyLanguage.h
@@ -0,0 +1,19 @@
+//
+//  BodyLanguage.h
+//  BodyLanguage
+//
+//  Created by Matias Piipari on 13/05/2016.
+//  Copyright © 2016 Matias Piipari & Co. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BodyLanguage.
+FOUNDATION_EXPORT double BodyLanguageVersionNumber;
+
+//! Project version string for BodyLanguage.
+FOUNDATION_EXPORT const unsigned char BodyLanguageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BodyLanguage/PublicHeader.h>
+
+
